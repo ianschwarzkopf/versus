7da26dbd25cf4fa701bacf76ac47898ea3d4e1ed
ianschwarzkopf <131293503+ianschwarzkopf@users.noreply.github.com>
2025-05-26 15:52:46 -0400

    login callback

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -5,11 +5,11 @@ export default function Login() {
     await supabase.auth.signInWithOAuth({
       provider: 'spotify',
       options: {
-      redirectTo: `${window.location.origin}/callback`,
+      redirectTo: `https://versus-plum.vercel.app//callback`,
     },
     });
   };
-
+  console.log(`${window.location.origin}/callback`);
 
   return <button onClick={signInWithSpotify}>Login with Spotify</button>;
 }
